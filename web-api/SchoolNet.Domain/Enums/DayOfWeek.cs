b35c8500fd4ff7850a46d597c272a0efa0ecd5ad
Egor <arnoldtagirov1@gmail.com>
2026-09-21 18:35:00 +0200

    Renamed class, added basic enums for entities

---
+++ web-api/SchoolNet.Domain/Enums/DayOfWeek.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolNet.Domain.Enums
+{
+    public enum DayOfWeek
+    {
+        Monday,
+        Tuesday,
+        Wednsday,
+        Thursday,
+        Friday
+    }
+}
