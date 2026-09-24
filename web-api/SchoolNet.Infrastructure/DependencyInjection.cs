cbe3177b94de915b5892b1019474eff51639167b
Egor <arnoldtagirov1@gmail.com>
2026-09-24 14:30:56 +0200

    Added connection string in infrastructure layer

--- web-api/SchoolNet.Infrastructure/DependencyInjection.cs
+++ web-api/SchoolNet.Infrastructure/DependencyInjection.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SchoolNet.Domain.Interfaces.Repository;
+using SchoolNet.Infrastructure.Persistence;
 using SchoolNet.Infrastructure.Repositories;
 
 namespace SchoolNet.Infrastructure
 {
     public static  class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            });
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<ISubjectRepository, SubjectRepository>();
             services.AddScoped<IScheduleRepository, SheduleRepository>();
--- web-api/web-api/Program.cs
+++ web-api/web-api/Program.cs
@@ -1,5 +1,6 @@
 
 using SchoolNet.Application;
+using SchoolNet.Infrastructure;
 
 namespace web_api
 {
@@ -15,6 +16,7 @@ namespace web_api
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
             builder.Services.AddApplication();
+            builder.Services.AddInfrastructure(builder.Configuration);
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
